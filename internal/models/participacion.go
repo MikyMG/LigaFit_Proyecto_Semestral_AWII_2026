36e2e7320adf297d31f595c2f47fb61383da2043
Karen Anchundia <anchundiakaren005@gmail.com>
2026-06-02 01:09:04 -0500

    feat: crear modelos competencia y participacion

--- internal/models/participacion.go
+++ internal/models/participacion.go
@@ -1,1 +1,14 @@
 package models
+
+import "time"
+
+type Participacion struct {
+	ID               int       `json:"id"`
+	CompetenciaID    int       `json:"competencia_id"`
+	DeportistaID     int       `json:"deportista_id"`
+	GrupoID          int       `json:"grupo_id"`
+	FechaInscripcion string    `json:"fecha_inscripcion"`
+	Estado           string    `json:"estado"`
+	CreatedAt        time.Time `json:"created_at"`
+	UpdatedAt        time.Time `json:"updated_at"`
+}
