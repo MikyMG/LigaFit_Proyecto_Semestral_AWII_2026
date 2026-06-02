a8ee35360fc98cf4f95394ee9ade5d68601942a7
Johantony2309 <johantony231@gmail.com>
2026-06-01 21:20:07 -0500

    feat: registrar rutas de seguimiento fisico

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -1,1 +1,18 @@
-package models
+package routes
+
+import (
+	"github.com/go-chi/chi/v5"
+
+	"LigaFit-AWII2026/internal/handlers"
+)
+
+func RegisterRoutes(r chi.Router) {
+	r.Route("/api/v1/seguimientos", func(r chi.Router) {
+		r.Post("/", handlers.CrearSeguimientoHandler)
+		r.Get("/", handlers.ObtenerSeguimientosHandler)
+		r.Get("/{id}", handlers.ObtenerSeguimientoPorIDHandler)
+		r.Get("/deportista/{deportista_id}", handlers.ObtenerSeguimientosPorDeportistaHandler)
+		r.Put("/{id}", handlers.ActualizarSeguimientoHandler)
+		r.Delete("/{id}", handlers.EliminarSeguimientoHandler)
+	})
+}
