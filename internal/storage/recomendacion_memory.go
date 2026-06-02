e5d3a4469710623891b1a3a7d7d3e21981ed3a67
Johantony2309 <johantony231@gmail.com>
2026-06-01 20:07:12 -0500

    feat: crear almacenamiento en memoria para seguimiento y recomendaciones

--- internal/storage/recomendacion_memory.go
+++ internal/storage/recomendacion_memory.go
@@ -1,1 +1,6 @@
-package models
+package storage
+
+import "LigaFit-AWII2026/internal/models"
+
+var Recomendaciones []models.RecomendacionNutricional
+var RecomendacionIDCounter = 1
--- internal/storage/seguimiento_memory.go
+++ internal/storage/seguimiento_memory.go
@@ -1,1 +1,6 @@
-package models
+package storage
+
+import "LigaFit-AWII2026/internal/models"
+
+var Seguimientos []models.SeguimientoFisico
+var SeguimientoIDCounter = 1
