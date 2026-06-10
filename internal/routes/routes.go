0bb74ea3e06a42fb9b6d8fdf387957e374398925
MikyMG <mogamasi16@gmail.com>
2026-06-09 22:19:00 -0500

    feat: registrar rutas de deportistas

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -1,1 +1,17 @@
-package models
+package routes
+
+import (
+	"github.com/go-chi/chi/v5"
+
+	"LigaFit-AWII2026/internal/handlers"
+)
+
+func RegisterRoutes(r chi.Router) {
+	r.Route("/api/v1/deportistas", func(r chi.Router) {
+		r.Post("/", handlers.CrearDeportistaHandler)
+		r.Get("/", handlers.ObtenerDeportistasHandler)
+		r.Get("/{id}", handlers.ObtenerDeportistaPorIDHandler)
+		r.Put("/{id}", handlers.ActualizarDeportistaHandler)
+		r.Delete("/{id}", handlers.EliminarDeportistaHandler)
+	})
+}
